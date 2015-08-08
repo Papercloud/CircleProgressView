9f81393f228575e486957a9238b6c5630d1ebf12
bphenriques <brunoaphenriques@gmail.com>
2015-08-08 10:46:50 +0100

    First commit

---
+++ CircularProgressView/CircularProgressView.h
@@ -0,0 +1,19 @@
+//
+//  CircularProgressView.h
+//  CircularProgressView
+//
+//  Created by Bruno Henriques on 08/08/15.
+//  Copyright (c) 2015 Bruno Henriques. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CircularProgressView.
+FOUNDATION_EXPORT double CircularProgressViewVersionNumber;
+
+//! Project version string for CircularProgressView.
+FOUNDATION_EXPORT const unsigned char CircularProgressViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircularProgressView/PublicHeader.h>
+
+
